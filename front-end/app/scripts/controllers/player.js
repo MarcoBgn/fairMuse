12a04b9686f0c73ef9e61bd67ff00376864e8ca4
Yan-Yi Li <liyanyi@gmail.com>
2016-03-23 14:38:20 +0000

    can select and play back songs

--- front-end/app/scripts/controllers/player.js
+++ front-end/app/scripts/controllers/player.js
@@ -1,15 +1,46 @@
 'use strict';
 
 fairMuse.controller('PlayerCtrl',
-       ["$sce", function ($sce) {
-           this.config = {
-               sources: [
-             {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/audios/videogular.mp3"), type: "audio/mpeg"},
-             {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/audios/videogular.ogg"), type: "audio/ogg"}
-         ],
-               theme: {
+  ["$sce", function ($sce) {
+    this.songList = [
+      {
+        songId: 1,
+        sources: [
+          {
+            src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/audios/videogular.mp3"),
+            type: "audio/mpeg"
+          }
+        ]
+      },
+      {
+        songId: 2,
+        sources: [
+          {
+            src: "songs/testSong.mp3",
+            type: "audio/mpeg"
+          }
+        ]
+      }
+    ];
+
+    this.config = {
+      theme: {
          url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
-               }
-           };
-       }]
-   );
+      }
+    };
+
+    var currentSong;
+
+    this.getSong = function(id) {
+     for(var i = 0; i < this.songList.length; i++) {
+       if(this.songList[i].songId === id) {
+         return currentSong = this.songList[i].sources;
+       };
+     };
+    };
+
+    this.changeSource = function(id){
+      this.config.sources = this.getSong(id);
+    }
+  }]
+);
