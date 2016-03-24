e0dbe2cbb5ad5291d231b63d52627e140bf05552
Yan-Yi Li <liyanyi@gmail.com>
2016-03-24 10:29:18 +0000

    removed extra 'end'

--- back-end/app/controllers/charges_controller.rb
+++ back-end/app/controllers/charges_controller.rb
@@ -1,31 +1,29 @@
 class ChargesController < ApplicationController
-
-  def index 
+  def index
   end
 
   def new
   end
-  
+
   def index
   end
-  
+
   def create
     @amount = 50
 
     customer = Stripe::Customer.create(
-      email: params[:stripeEmail], 
+      email: params[:stripeEmail],
       source: params[:stripeToken])
 
     @charge = Stripe::Charge.create(
-      customer: customer.id, 
+      customer: customer.id,
       amount: @amount,
       description: 'artist contribution',
       currency: 'gbp')
 
-      redirect_to 'http://localhost:9000/#/about'
-    rescue Stripe::CardError => error
-      flash[:error] = error.message
-      redirect_to new_charge_path
-    end
+    redirect_to 'http://localhost:9000/#/about'
+  rescue Stripe::CardError => error
+    flash[:error] = error.message
+    redirect_to new_charge_path
   end
 end
