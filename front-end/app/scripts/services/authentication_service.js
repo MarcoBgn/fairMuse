68a8829e775e610cc79f94775ee200369201c5a3
Mark Hill <futonfutonda@gmail.com>
2016-03-25 17:05:42 +0000

    gave nav bar own view

--- front-end/app/scripts/services/authentication_service.js
+++ front-end/app/scripts/services/authentication_service.js
@@ -1,12 +1,14 @@
 angular.module('fairMuseApp').service('authenticationService', function($http, $location){
-	var location = $location	
+	var location = $location
 		this.login= function(user) {
-      console.log(user)
-		return $http.post("http://localhost:3000/api/login", user); 
+		return $http.post("http://localhost:3000/api/login", user);
 		};
-	
+
 		this.isLoggedIn = function(){
 			return (localStorage.getItem('auth_token')) ? true : false;
 		};
+		this.LogOut = function(){
+			return $http.delete("http://localhost:3000/api/logout")
+		}
 
 	});
