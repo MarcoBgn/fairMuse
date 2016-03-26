0ed14e085d98d4786a8a10e24b800942e58a4fc4
russellvaughan <russellvaughan@gmail.com>
2016-03-26 08:46:12 +0000

    logout test added and successful to the authservicde unit test

--- front-end/test/spec/services/authenticate_service.js
+++ front-end/test/spec/services/authenticate_service.js
@@ -3,8 +3,8 @@ describe('Controller: authenticationService', function(){
 
   beforeEach(function(){
       
-      beforeEach(module('fairMuseApp'));
-      beforeEach(module('ui.bootstrap'));
+      module('fairMuseApp');
+      module('ui.bootstrap');
 
     inject(function(_$httpBackend_, $location, _authenticationService_){
       user = {}
@@ -12,16 +12,25 @@ describe('Controller: authenticationService', function(){
       authenticationService = _authenticationService_
       httpBackend.expectGET('views/main.html').respond(200)
       httpBackend.whenPOST('http://localhost:3000/api/login', params).respond(200)
-      httpBackend.whenGET('views/songs.html').respond(200)
+      httpBackend.whenDELETE('http://localhost:3000/api/logout.json').respond(200)
       location = $location
       params = {"name":"new space"}
     });
   });
 
-  it('sends a post to the backend', function(){
+
+  it('has a login function that sends a post to the backend', function(){
     httpBackend.flush()
     authenticationService.login()
-    expect(httpBackend.flush).not.toThrow();
-  })
+    expect(httpBackend.flush).not.toThrow();    
+  });
+
+   it('has a logout function that sends a delete to the backend', function(){
+    httpBackend.flush()
+    authenticationService.LogOut()
+    expect(httpBackend.flush).not.toThrow();    
+  });
 
 });
+
+
