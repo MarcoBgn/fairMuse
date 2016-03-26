8b2fa8b956661c722c4fb1eb4c7cc0cd965ba715
russellvaughan <russellvaughan@gmail.com>
2016-03-26 18:44:22 +0000

    user_spec created. unit test for auth token generator, created and passes

--- back-end/spec/models/user_spec.rb
+++ back-end/spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user){described_class.new}
+
+  it "generates an authentication token" do
+    allow(user).to receive(:email).and_return 'dave@gmail.com'
+    allow(user).to receive(:password).and_return 'password'
+    allow(Devise).to receive(:friendly_token).and_return :token
+    expect(user.ensure_authentication_token).to eq "token"
+  end
 end
