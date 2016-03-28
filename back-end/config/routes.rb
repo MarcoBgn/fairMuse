1ee5760e6cf44fa301f6e824dcdc02c0b2898886
Mark Hill <futonfutonda@gmail.com>
2016-03-28 21:29:14 +0100

    artists can sign up

--- back-end/config/routes.rb
+++ back-end/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :artists
   resources :charges
   resources :tracks
   resources :users, only: [:index]
   devise_for :users, skip: [:sessions, :registrations]
   as :user do
+    post '/api/artistsignup' => 'artistreg#create'
     post '/api/signup' => 'registrations#create'
     post '/api/login' => 'sessions#create'
     delete '/api/logout' => 'sessions#destroy'
