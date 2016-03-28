ee04c5c34b06fa876d093a3cd797b246565a5b63
Mark Hill <futonfutonda@gmail.com>
2016-03-28 20:47:53 +0100

    finished front end for artist signup

--- front-end/app/scripts/services/signup_service.js
+++ front-end/app/scripts/services/signup_service.js
@@ -3,4 +3,7 @@ angular.module('fairMuseApp').service('signupService', function($http, $location
     this.signup= function(user) {
     return $http.post("http://localhost:3000/api/signup", user);
     };
+    this.artistSignup= function(user) {
+    return $http.post("http://localhost:3000/api/artistsignup", user);
+    };
   });
