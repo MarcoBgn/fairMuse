29becc972b10433d94ee512693075b3092e698c2
Marco Bagnasco <admin@hecticmojo.com>
2016-03-28 10:50:30 +0100

    WIP 6-track-song-plays
    - Updated the controller with find_or_create_by method
    - Created private methods total_count and weekly_count in streams_controller.rb

--- back-end/app/controllers/streams_controller.rb
+++ back-end/app/controllers/streams_controller.rb
@@ -1,13 +1,41 @@
 class StreamsController < ApplicationController
   
   def create
-    track = Treack.get(params[:track_id])
-    stream = Stream.create(track_id: track.id, user_id: current_user.id, total_plays: )
+    Stream.find_or_create_by(track_id: params[:track_id], user_id: user_id) do |stream|
+      stream.total_plays = total_count
+      stream.weekly_plays = weekly_count
+      stream.update(total_plays: total_count, weekly_plays: weekly_count)
+      stream.save
+    end
+    
+    
+                          
+    render json:{}, status: :ok
   end
   
   private
   
-  def plays_count
-    track = Track.get(params[:track_id])
+  def total_count
+    stream = Stream.find(track_id: params[:track_id], user_id: user_id)
+    if stream.total_plays
+     return stream.total_plays + 1
+    end
+    return 1
   end
+  
+  def weekly_count
+    stream = Stream.find(track_id: params[:track_id], user_id: user_id)
+    if stream.weekly_plays
+      return stream.weekly_plays + 1
+    end
+    return 1
+  end
+  
+  def user_id
+    if current_user
+      current_user.id
+    end
+      return 1
+  end
+  
 end
