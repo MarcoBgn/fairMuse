c1737ef8202c0f9a3f591be904b6b9677c6cfb3e
russellvaughan <russellvaughan@gmail.com>
2016-03-28 19:43:25 +0100

    amended feature tests for streaming a track

--- back-end/spec/features/streams_a_track_spec.rb
+++ back-end/spec/features/streams_a_track_spec.rb
@@ -1,24 +1,19 @@
-ENV['RAILS_ENV'] = 'development'
-
 feature 'Tracks the number of plays a song has' do
   
-  after(:each) do
-  end
-  
+#defering feature tests until after implemnetation of user story 10: play uploaded music.
+
   scenario 'Logged in user plays a song already streamed', js: true do
-    p "+++++++#{Stream.count}"
-    User.create(email:'admin@gmail.com', password: '12345678')
+    Stream.find_by(track_id: 1).destroy!
     visit 'http://localhost:9000/'
     log_in
     expect{click_button('Play Yan-Yi\'s song')}.not_to change(Stream, :count)
     find(:css, "#log_out").click
+    sleep(2)
   end
   
-  scenario 'Logged in user plays a song for the first time', js: true do
-      p "+++++++#{Stream.count}"
-    User.create(email:'admin@gmail.com', password: '12345678')
-    visit 'http://localhost:9000/'
-    log_in
-    expect{click_button('Play default song')}.to change(Stream, :count)
+  scenario 'Logged in user plays a song for the first time', js: true do    
+    visit 'http://localhost:9000/#/songs'
+    click_button('Play default song')
+    expect(Stream.count).to eq(2)
   end
 end
