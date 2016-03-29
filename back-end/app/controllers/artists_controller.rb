bb3f1043d55b8ecfa29ba3b65098667f973f5b56
Mark Hill <futonfutonda@gmail.com>
2016-03-29 11:03:32 +0100

    indentation

--- back-end/app/controllers/artists_controller.rb
+++ back-end/app/controllers/artists_controller.rb
@@ -1,5 +1,6 @@
 class ArtistsController < ApplicationController
   before_filter :authenticate_user_from_token
+  
   def index
     if current_user
       render nothing: true
@@ -7,4 +8,5 @@ class ArtistsController < ApplicationController
       render json:{}, status: :unauthorized
     end
   end
+
 end
--- front-end/app/scripts/controllers/artistsignin.js
+++ front-end/app/scripts/controllers/artistsignin.js
@@ -1,24 +1,24 @@
 angular.module('fairMuseApp')
-  .controller('ArtistSigninCtrl', function(authenticationService, flash, $location) {
-   var self = this
-    var user, email, password, authenticationService, response, location;
-   location = $location
-   self.email = "";
-    self.password = "";
+.controller('ArtistSigninCtrl', function(authenticationService, flash, $location) {
+  var self = this
+  var user, email, password, authenticationService, response, location;
+  location = $location
+  self.email = "";
+  self.password = "";
 
-     self.sendForm = function(email, password) {
-       self.user = {email: self.email,
-                    password: self.password}
-        var promise = authenticationService.artistLogin(self.user);
-       promise.then(success, error);
+  self.sendForm = function(email, password) {
+    self.user = {email: self.email,
+      password: self.password}
+      var promise = authenticationService.artistLogin(self.user);
+      promise.then(success, error);
     };
 
-       var success = function(response){
-       localStorage.setItem('auth_token', response.data.auth_token);
-       location.path('/songs')
-       };
+    var success = function(response){
+      localStorage.setItem('auth_token', response.data.auth_token);
+      location.path('/songs')
+    };
 
-       var error = function(response) {
-       flash('Wrong Login Credentials!');
-     };
-});
+    var error = function(response) {
+      flash('Wrong Login Credentials!');
+    };
+  });
