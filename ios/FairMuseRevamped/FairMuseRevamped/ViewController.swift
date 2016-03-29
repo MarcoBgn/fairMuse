988876d77011eb968ba3cd91947217af6dcb1bb5
David Parry <drjparry@gmail.com>
2016-03-29 16:14:38 +0200

    WIP added table view

--- ios/FairMuseRevamped/FairMuseRevamped/ViewController.swift
+++ ios/FairMuseRevamped/FairMuseRevamped/ViewController.swift
@@ -11,12 +11,50 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var tableData = []
-
+    
+    
+    
     @IBOutlet weak var AppsTableView: UITableView!
+   
+    func searchItunesFor(searchTerm: String) {
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        // Now escape anything else that isn't URL-friendly
+        if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
+            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+            let url = NSURL(string: urlPath)
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithURL(url!, completionHandler: {data, response, error -> Void in
+                print("Task completed")
+                if(error != nil) {
+                    // If there is an error in the web request, print it to the console
+                    print(error!.localizedDescription)
+                }
+                
+                if let jsonResult = try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableLeaves) as? NSDictionary {
+                   
+                    if let results: NSArray = jsonResult["results"] as? NSArray {
+                        dispatch_async(dispatch_get_main_queue(), {
+                            self.tableData = results
+                            self.AppsTableView!.reloadData()
+                        })
+                    }
+                }
+            })
+            
+            // The task is just an object with all these properties set
+            // In order to actually make the web request, we need to "resume"
+            task.resume()
+        }
+    }
+
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        searchItunesFor("JQ Software")
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,15 +62,31 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Dispose of any resources that can be recreated.
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section:    Int) -> Int {
-        return 10
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
     }
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
         
-        cell.textLabel?.text = "Row #\(indexPath.row)"
-        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
-        
+        if let rowData: NSDictionary = self.tableData[indexPath.row] as? NSDictionary,
+            // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+            urlString = rowData["artworkUrl60"] as? String,
+            // Create an NSURL instance from the String URL we get from the API
+            imgURL = NSURL(string: urlString),
+            // Get the formatted price string for display in the subtitle
+            formattedPrice = rowData["formattedPrice"] as? String,
+            // Download an NSData representation of the image at the URL
+            imgData = NSData(contentsOfURL: imgURL),
+            // Get the track name
+            trackName = rowData["trackName"] as? String {
+            // Get the formatted price string for display in the subtitle
+            cell.detailTextLabel?.text = formattedPrice
+            // Update the imageView cell to use the downloaded image data
+            cell.imageView?.image = UIImage(data: imgData)
+            // Update the textLabel text to use the trackName from the API
+            cell.textLabel?.text = trackName
+        }
         return cell
     }
 
