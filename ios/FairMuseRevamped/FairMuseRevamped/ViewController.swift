8c702633d09b0749980ebdc1df1180c206bf79bf
David Parry <drjparry@gmail.com>
2016-03-29 12:57:28 +0200

    restarted build to reflect new prototype with two main views

---
+++ ios/FairMuseRevamped/FairMuseRevamped/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  FairMuseRevamped
+//
+//  Created by David Parry on 29/03/2016.
+//  Copyright © 2016 David Parry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var tableData = []
+
+    @IBOutlet weak var AppsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section:    Int) -> Int {
+        return 10
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        cell.textLabel?.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        
+        return cell
+    }
+
+
+}
+
