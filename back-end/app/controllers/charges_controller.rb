919f8c3f42b9d3a8fa6bb9fdbaab688346f36b73
David Parry <drjparry@gmail.com>
2016-03-30 23:58:47 +0200

    added 2 routes to charge controller: 1 for contribution and 1 for subscriptions

--- back-end/app/controllers/charges_controller.rb
+++ back-end/app/controllers/charges_controller.rb
@@ -7,18 +7,18 @@ class ChargesController < ApplicationController
   end
 
   def create
-    @amount = 50
-    user = User.find(params[:user_id])
-    user.balance = 9.99
-    p user.balance 
-    p user
+    amount = params[:amount].to_i
+    if_user(params[:user_id])
+    @user.balance = 0
+    amount ? @user.balance += amount : @user.balance = 9
+
     customer = Stripe::Customer.create(
-      email: params[:stripeEmail],
-      source: params[:stripeToken])
+    email: params[:stripeEmail],
+    source: params[:stripeToken])
 
     @charge = Stripe::Charge.create(
       customer: customer.id,
-      amount: @amount,
+      amount: amount ? amount : 900,
       description: 'artist contribution',
       currency: 'gbp')
 
@@ -26,5 +26,15 @@ class ChargesController < ApplicationController
   rescue Stripe::CardError => error
     flash[:error] = error.message
     redirect_to new_charge_path
+  end
+
+  private
+
+  def if_user(params_user_id)
+    if params_user_id
+      @user = User.find(params_user_id)
+    else
+      @user = User.find(0)
+    end
   end
 end
