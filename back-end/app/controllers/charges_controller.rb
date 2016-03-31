4662599245c210e61fac8b2b80623606d7a9999c
Marco Bagnasco <admin@hecticmojo.com>
2016-03-31 11:57:10 +0100

    Refactored charges controller
    Removed about page from artists view
    Added the navbar to the success page

--- back-end/app/controllers/charges_controller.rb
+++ back-end/app/controllers/charges_controller.rb
@@ -1,5 +1,6 @@
 class ChargesController < ApplicationController
-
+  SUBSCRIPTION_AMOUNT = 900
+  
   def new
   end
 
@@ -7,33 +8,35 @@ class ChargesController < ApplicationController
   end
 
   def create
-    @amount = params[:amount]
-    if_user(params[:user_id])
-    @user.balance = 0
-    @amount ? @user.balance += @amount.to_i : @user.balance = 9
+    parse_amount(params[:amount])
+    user_type(params[:user_id])
     stripe(params[:stripeEmail], params[:stripeToken])
   end
 
   private
 
-  def if_user(params_user_id)
-    if params_user_id
-      @user = User.find(params_user_id)
-    else
-      @user = User.find(0)
+  def user_type(params_id)
+    if params_id
+      @user = User.find(params_id)
     end
+      @user = User.find(0)
+  end
+  
+  def parse_amount(amount)
+    @amount = amount.to_i * 100
   end
 
   def stripe(email, token)
     customer = Stripe::Customer.create(
       email: email,
       source: token)
-
+      
     @charge = Stripe::Charge.create(
       customer: customer.id,
-      amount: @amount ? @amount.to_i : 900,
+      amount: @amount ? @amount : SUBSCRIPTION_AMOUNT,
       description: 'artist contribution',
       currency: 'gbp')
+      @amount ? @user.balance += @amount : @user.balance = SUBSCRIPTION_AMOUNT
 
     redirect_to 'http://localhost:9000/#/success'
   rescue Stripe::CardError => error
