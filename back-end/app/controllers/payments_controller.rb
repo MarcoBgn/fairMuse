7cb283c599e4931e138f812e3539dd69e0b614f5
Marco Bagnasco <admin@hecticmojo.com>
2016-03-31 12:14:58 +0100

    WIP refactoring payments_controller.rb

--- back-end/app/controllers/payments_controller.rb
+++ back-end/app/controllers/payments_controller.rb
@@ -2,27 +2,25 @@ class PaymentsController < ApplicationController
 
   def pay
     user = User.find_by(id: params[:user_id])
-    user_balance = user.balance || 0
-    new_balance = user_balance - 100
+    new_balance = user.balance - 100
 
     user.update(balance: new_balance)
 
-    tot_plays = 0
-    track_num =[]
-    track_plays = []
+    tot_plays = 25
+    track_num =[2, 1, 6]
+    track_plays = [3, 2, 20]
     streams = Stream.where(user_id: params[:user_id]).each do |stream|
-      tot_plays += stream.total_plays
+      tot_plays += stream.weekly_plays
       track_num << stream.track_id
-      track_plays << stream.total_plays
+      track_plays << stream.weekly_plays
     end
-    payments = track_plays.map do |plays|
+    payments = track_plays.map do |plays|     #[12, 8, 80]
       plays.to_f/tot_plays * 100
     end
     track_num.each_with_index do |track, index|
       artist_id = Track.find_by(id: track).artist_id
       artist = Artist.find_by(id: artist_id)
-      cur_balance = artist.balance || 0
-      new_balance = cur_balance + payments[index]
+      new_balance = artist.balance + payments[index]
       artist.update(balance: new_balance)
     end
     render json:{message: "default message"}
