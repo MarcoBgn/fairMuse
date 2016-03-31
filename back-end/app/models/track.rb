3ba44edf11fcd5f0ec559aec2e91499d9953a867
Marco Bagnasco <admin@hecticmojo.com>
2016-03-31 09:34:11 +0100

    Fixed Image upload on tracks

--- back-end/app/models/track.rb
+++ back-end/app/models/track.rb
@@ -1,7 +1,5 @@
 class Track < ActiveRecord::Base
   has_attached_file :file
-  has_many :streams, dependent: :destroy
-  belongs_to :artist
   validates_attachment :file, presence: true,
   content_type: { content_type: /\Aaudio\/.*\Z/}, 
   size: { in: 0..10.megabytes }
@@ -9,4 +7,17 @@ class Track < ActiveRecord::Base
   def track_url
     file.url
   end
+  
+  has_attached_file :image, styles: {
+    thumb: '200x200>',
+    medium: '500x500>'
+  }
+  validates_attachment_content_type :image , :content_type => /\Aimage\/.*\Z/
+  
+  def image_url
+    image.url(:thumb)
+  end
+  
+  has_many :streams, dependent: :destroy
+  belongs_to :artist
 end
