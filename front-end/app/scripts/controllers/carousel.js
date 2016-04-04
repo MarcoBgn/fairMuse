2f52904a021b247af396336c9622c32992f36b8d
Marco Bagnasco <admin@hecticmojo.com>
2016-04-04 20:02:55 +0100

    Fixed Carousel / modified Css

--- front-end/app/scripts/controllers/carousel.js
+++ front-end/app/scripts/controllers/carousel.js
@@ -5,29 +5,20 @@ fairMuse.controller('MyCarouselController', function () {
   this.myInterval = 5000;
   this.noWrapSlides = false;
   this.active = 0;
-  var pics = ["http://bsrv.wallpapersten.com/media/wp/1-17-4/brunette-with-headphones-enjoying-the-music-1080p-girl-desktop-wallpaper-27464.jpg", "http://img.gawkerassets.com/img/17vzbduvh99gnjpg/original.jpg", "https://whileyouweresleeping.files.wordpress.com/2015/04/maxresdefault.jpg", "http://c8.alamy.com/comp/AXPJ9C/a-steel-worker-in-front-of-a-blast-furnace-in-benxi-iron-and-steel-AXPJ9C.jpg"]
+  var pics = ["http://bsrv.wallpapersten.com/media/wp/1-17-4/brunette-with-headphones-enjoying-the-music-1080p-girl-desktop-wallpaper-27464.jpg", "http://img.gawkerassets.com/img/17vzbduvh99gnjpg/original.jpg", "https://whileyouweresleeping.files.wordpress.com/2015/04/maxresdefault.jpg"]
 
   var slides = this.slides = [];
   var currIndex = 0;
 
   this.addSlide = function() {
-    var newWidth = 900 + slides.length + 1;
     slides.push({
       image: pics[currIndex],
-      text: ['Space','Night','Peace','Adventure'][slides.length % 4],
+      text: ['Place','Holder','For','Proposition'][slides.length % 4],
       id: currIndex++
     });
   };
 
-
-
-
-  this.randomize = function() {
-    var indexes = generateIndexesArray();
-    assignNewIndexesToSlides(indexes);
-  };
-
-  for (var i = 0; i < 4; i++) {
+  for (var i = 0; i < 3; i++) {
     this.addSlide();
   }
 });
