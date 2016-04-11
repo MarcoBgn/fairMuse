d9a0a4fc9babc8a505f3fb74a45a275a6eeaa30b
Mark Hill <futonfutonda@gmail.com>
2016-04-11 20:49:48 +0100

    place artist and user signup on the same page

--- front-end/app/scripts/controllers/artistSignup.js
+++ front-end/app/scripts/controllers/artistSignup.js
@@ -10,27 +10,6 @@ angular.module('fairMuseApp')
     this.artistId = localStorage.getItem('artist_id')
     this.artistName = localStorage.getItem('name')
 
-    self.sendSignUpForm = function() {
-       self.artistInfo = {name: self.name,
-                    email: self.email,
-                    password: self.password,
-                    password_confirmation: self.password_confirmation}
-       var promise = signupService.artistSignup(self.artistInfo);
-      promise.then(success, error);
-    };
-
-      var success = function(response){
-      localStorage.setItem('auth_token', response.data.auth_token);
-      localStorage.setItem('artist_id', response.data.artist_id);
-      localStorage.setItem('is_artist', response.data.is_artist);
-      localStorage.setItem('name', response.data.name);
-
-      location.path('/artistaccount')
-      };
-
-      var error = function(response) {
-        flash('Sign up failed!');
-     };
 
      self.displayBalance = function(){
            var promise = self.getBalance();
