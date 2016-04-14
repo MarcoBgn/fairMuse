611e31a2ab44d37acc484dfb5ae566fb93090590
Mark Hill <futonfutonda@gmail.com>
2016-04-14 12:43:44 +0100

    feature tests passing for log in and out

--- back-end/spec/features/sign_in_spec.rb
+++ back-end/spec/features/sign_in_spec.rb
@@ -1,6 +1,6 @@
 feature 'Sign in' do
   before do
-    User.create(email:'admin@gmail.com', password: '12345678')
+    User.create(email:'admin@gmail.com', password:'12345678')
   end
 
   after(:each) do
@@ -11,9 +11,10 @@ feature 'Sign in' do
     visit 'http://localhost:9000/'
     click_link('Log in')
     find(:css, ".user_tab").click
-    fill_in 'email', with: 'admin@gmail.com'
-    fill_in 'password', with: '12345678'
+    fill_in 'email', with:'admin@gmail.com'
+    fill_in 'password', with:'12345678'
     click_button('Log in')
+    sleep(1)
     visit 'http://localhost:9000/'
     expect(page).to have_content('Log out')
     find(:css, "#log_out").click
@@ -21,20 +22,26 @@ feature 'Sign in' do
 
   scenario 'allows a user to sign out', js: true do
     visit 'http://localhost:9000/'
+    log_in
+    visit 'http://localhost:9000/'
+    find(:css, "#log_out").click
     expect(page).to_not have_content('Log out')
   end
 
   scenario "doesn't allow a user to sign in without an account", js: true do
     visit 'http://localhost:9000/'
     click_link('Log in')
+    find(:css, ".user_tab").click
     fill_in 'email', with: 'noaccount@gmail.com'
     fill_in 'password', with: '12345678'
     click_button('Log in')
+    sleep(1)
     expect(page).to_not have_content('Log out')
   end
   scenario "link to user account page is displayed when logged in", js: true do
     visit 'http://localhost:9000/'
     log_in
+    visit 'http://localhost:9000/'
     click_link('Account')
     expect(page).to have_content('User Account')
     find(:css, "#log_out").click
--- back-end/spec/login_helper.rb
+++ back-end/spec/login_helper.rb
@@ -1,9 +1,10 @@
 def log_in(email='admin@gmail.com', password='12345678')
   click_link('Log in')
-  find(:css, "#user_tab").click
+  find(:css, ".user_tab").click
   fill_in 'email', with: email
   fill_in 'password', with: password
   click_button('Log in')
+  sleep(1)
 end
 
 def artist_log_in(email='name@email.com', password='12345678')
