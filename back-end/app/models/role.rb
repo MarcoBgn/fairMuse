429cfa5a3ea730add50251fadaac5fa46267b2bd
Mark Hill <futonfutonda@gmail.com>
2016-04-19 15:56:23 +0100

    added methods to check for roles

--- back-end/app/models/role.rb
+++ back-end/app/models/role.rb
@@ -1,2 +1,10 @@
 class Role < ActiveRecord::Base
+  belongs_to :user
+
+  def check_artist(id)
+    Role.where(user_id: id).artist
+  end
+  def check_subscriber(id)
+    Role.where(user_id: id).subscriber
+  end
 end
